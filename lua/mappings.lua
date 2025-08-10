dd8fb066b7d480c89d9a951a68be6d27993db9bb
Alex Forshtat <forshtat1@gmail.com>
2025-08-10 22:48:36 +0000

    Add hotkey to accept suggestion (works sometimes - tbd)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,4 +10,11 @@ map("i", "jk", "<ESC>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", "<leader>i", function()
+  vim.lsp.buf.code_action({
+    apply = true
+  })
+end, { desc = "Auto-import first suggestion" })
+
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
