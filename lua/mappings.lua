0382f1b0bbcd93b8035542a5e911d673f3cd36fe
Alex Forshtat <forshtat1@gmail.com>
2025-08-10 21:57:59 +0200

    Remap <C-d> to <C-d>zz

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
