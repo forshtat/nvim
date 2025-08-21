f9acd1cb7f310bef612a669e2073c22d103e3ae7
Alex Forshtat <forshtat1@gmail.com>
2025-08-21 21:04:39 +0200

    On MacOS use the login shell in terminal panel

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,12 @@ require "nvchad.options"
 
 -- add yours here!
 
+-- Only configure shell on macOS
+if vim.loop.os_uname().sysname == "Darwin" then
+  -- Get macOS login shell
+  local login_shell = vim.fn.trim(vim.fn.system("dscl . -read ~/ UserShell | awk '{print $2}'"))
+  -- Add -l so it runs as a login shell
+  vim.env.SHELL = login_shell .. " -l"
+end
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
